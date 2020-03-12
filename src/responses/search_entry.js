1c22f1e85ae30cea832070fd4acc5cfff1bc2e54
Francisco Mora <francisco.mora.sanchez@gmail.com>
2020-03-12 11:32:26 +0100

    changed search_entry in responses to get valid attribute jpegPhoto from ldap

--- src/responses/search_entry.js
+++ src/responses/search_entry.js
@@ -1,32 +1,48 @@
-const assert = require('assert-plus');
-const Response = require('./response');
-const Attribute = require('../attribute');
-const { LDAP_REP_SEARCH_ENTRY } = require('../utils/protocol');
+const assert = require('assert-plus')
+const Response = require('./response')
+const Attribute = require('../attribute')
+const { LDAP_REP_SEARCH_ENTRY } = require('../utils/protocol')
 
 module.exports = class extends Response {
-  constructor(options) {
-    super(Object.assign({ protocolOp: LDAP_REP_SEARCH_ENTRY, type: 'SearchEntry', attributes: [] }, options));
+  constructor (options) {
+    super(Object.assign({ protocolOp: LDAP_REP_SEARCH_ENTRY, type: 'SearchEntry', attributes: [] }, options))
   }
 
-  get object() {
+  // get object() {
+  //   return this.attributes.reduce((obj, a) => {
+  //     obj[a.type] = a.vals && a.vals.length ? a.vals.length > 1 ? a.vals.slice() : a.vals[0] : [];
+  //     return obj;
+  //   }, { dn: this.objectName });
+  // }
+
+  get object () {
     return this.attributes.reduce((obj, a) => {
-      obj[a.type] = a.vals && a.vals.length ? a.vals.length > 1 ? a.vals.slice() : a.vals[0] : [];
-      return obj;
-    }, { dn: this.objectName });
+      const buf = a.buffers
+      const val = a.vals
+      let item
+      if (a.type === 'jpegPhoto') {
+        item = buf
+      } else {
+        item = val
+      }
+      obj[a.type] = item && item.length ? item.length > 1 ? item.slice() : item[0] : []
+      // obj[a.type] = a.vals && a.vals.length ? a.vals.length > 1 ? a.vals.slice() : a.vals[0] : [];
+      return obj
+    }, { dn: this.objectName })
   }
 
-  parse(ber) {
-    this.objectName = ber.readString();
+  parse (ber) {
+    this.objectName = ber.readString()
 
-    assert.ok(ber.readSequence());
+    assert.ok(ber.readSequence())
 
-    const end = ber.offset + ber.length;
+    const end = ber.offset + ber.length
     while (ber.offset < end) {
-      const a = new Attribute();
-      a.parse(ber);
-      this.attributes.push(a);
+      const a = new Attribute()
+      a.parse(ber)
+      this.attributes.push(a)
     }
 
-    return true;
+    return true
   }
-};
+}
